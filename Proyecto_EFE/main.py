27f76b9fc97a3ff45735e0e04ec7c40c38388f19
nhhl00 <agustin8montenegro@gmail.com>
2025-11-06 20:47:35 -0300

    avance config

--- Proyecto_EFE/main.py
+++ Proyecto_EFE/main.py
@@ -1,1 +1,21 @@
-print("codigo de prueba")
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+#config:
+TITULO_VENTANA = "Gestion de Trenes"
+DIMENSION_VENTANA = "800x500"
+COLOR_VENTANA = "#f5f2f4"
+
+#ui:
+
+
+def main():
+    root = tk.Tk()
+    root.title(TITULO_VENTANA)
+    root.geometry(DIMENSION_VENTANA)
+    root.configure(bg=COLOR_VENTANA)
+
+    root.mainloop()
+
+main()
+
