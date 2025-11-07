ca11d2a390b36a0e490e1bc679e8431e774225bf
nhhl00 <agustin8montenegro@gmail.com>
2025-11-07 15:05:41 -0300

    ui avance

--- Proyecto_EFE/main.py
+++ Proyecto_EFE/main.py
@@ -3,17 +3,30 @@ from tkinter import ttk, messagebox
 
 #config:
 TITULO_VENTANA = "Gestion de Trenes"
-DIMENSION_VENTANA = "800x500"
+DIMENSION_VENTANA = "800x600"
 COLOR_VENTANA = "#f5f2f4"
 
-#ui:
 
+
+#ui(ventanas):
 
 def main():
     root = tk.Tk()
     root.title(TITULO_VENTANA)
     root.geometry(DIMENSION_VENTANA)
     root.configure(bg=COLOR_VENTANA)
+
+    #ui(ventana):
+    tk.Label(root, text="Sistema de gestion de tráfico ferroviario EFE Chile",bg="#f5f2f4",font=("Arial", 14)).pack(padx=50, pady=50)
+
+
+    #ui(botones):
+    boton_nueva_simulacion = ttk.Button(root, text="Nueva Simulación", command = lambda:print("hiciste click en nueva simulacion"))
+    boton_nueva_simulacion.pack(padx = 10, pady = 5)
+    boton_cargar_simulacion = ttk.Button(root, text="Cargar Simulación", command = lambda:print("hiciste click en cargar simulacion"))
+    boton_cargar_simulacion.pack(padx = 10, pady = 5)
+
+
 
     root.mainloop()
 
