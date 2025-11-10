fb4681316432a4d6c296a2283e03c99f962d2497
nhhl00 <agustin8montenegro@gmail.com>
2025-11-09 22:38:52 -0300

    xd

--- Proyecto_EFE/logic/SistemaDeGuardado.py
+++ Proyecto_EFE/logic/SistemaDeGuardado.py
@@ -1,10 +1,12 @@
-import tkinter as tk
-
-class SistemaDeGuardado:
-    def guardar_simulacion():
-        pass
-
-    def cargar_simulacion():
-        pass
+class SistemaGuardado:
+    def __init__(self, nombre_archivo: str):
+        self.nombre_archivo = nombre_archivo
 
 
+def guardar_simluacion():
+    pass
+
+
+def cargar_simulacion():
+    pass
+    
